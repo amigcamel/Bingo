2a451750618a228c15e713a5c3399c3448b481dc
amigcamel <amigcamel@gmail.com>
2020-12-07 23:55:33 +0800

    start to rewrite in Vue

--- bingo.js
+++ bingo.js
@@ -1,27 +1,3 @@
-function shuffle(array) {
-	var currentIndex = array.length,
-	temporaryValue, randomIndex;
-	while (0 !== currentIndex) {
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
-	return array;
-}
-
-function gen_nums() {
-	nums = []
-	for (var n = 1; n < 75; n++) {
-		nums.push(n)
-	}
-	nums = shuffle(nums)
-	$('.f').each(function (i) {
-		$(this).text(nums[i])
-	})
-}
-
 // http://stackoverflow.com/a/881147/1105489
 String.prototype.count=function(s1) { 
     return (this.length - this.replace(new RegExp(s1,"g"), '').length) / s1.length;
@@ -65,48 +41,42 @@ function cal() {
 }
 
 $(function () {
-	gen_nums();
 	$('body').on('touchstart click', '.f', function(e) {
 		e.preventDefault();
 		$(this).toggleClass('sel');
 		cal();
 	});
-	 setTimeout(function() {
-	 	$('#joke').removeClass('hide');
-	 }, 5000);
-	$('.container').on('touchstart click', '#joke', function(e) {
-		e.preventDefault();
-		tits = ['你確定', '想太多了'];
-		msgs = ['<h3>真的要放棄了嗎？</h3>', '<h3>哪有這麼好康的事情 = = </h3>'];
-		oks = ['我要放棄', '我錯了>"<'];
-		cns = ['不，我有運動家的精神', '關閉'];
+})
 
-        for(var i = 1; i >=0; i--) {
-            var dialog = new BootstrapDialog({
-            	type: BootstrapDialog.TYPE_DANGER,
-                title: tits[i],
-                message: msgs[i],
-                buttons: [{
-                    label: cns[i],
-                    action: function(){
-                        $.each(BootstrapDialog.dialogs, function(id, dialog){
-                            dialog.close();
-                        });
-                    }
-                }, {
-                    cssClass: 'btn-warning',
-                    label: oks[i],
-                    action: function(dialogRef){
-                        dialogRef.close();
-                    }
-                }]
-            });
-            dialog.open();
-        }
-		// BootstrapDialog.show({
-		// 	type: BootstrapDialog.TYPE_DANGER,
-		// 	title: '想太多了',
-		// 	message: '哪有這麼好康的事情...'
-		// });
-	})
+function getRandom(arr, n) {
+  // How to get a number of random elements from an array?
+  // https://stackoverflow.com/a/19270021/1105489
+  var result = new Array(n),
+     len = arr.length,
+     taken = new Array(len);
+  if (n > len)
+    throw new RangeError("getRandom: more elements taken than available");
+  while (n--) {
+    var x = Math.floor(Math.random() * len);
+    result[n] = arr[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
+}
+
+var app = new Vue({
+  el: '.cont',
+  data: {
+    prefixes: ['a', 'b', 'c', 'd', 'e'],
+    numbers: [1, 2, 3, 4, 5],
+    sids: getRandom(Object.keys(hsdic), 25),
+  },
+  methods: {
+    cellText: function(idx1, idx2) {
+      return this.sids[(idx1 * 5) + idx2]
+    },
+    cellImg: function(idx1, idx2) {
+      return "https://lh3.googleusercontent.com/a-/" + hsdic[this.sids[(idx1 * 5) + idx2]]
+    },
+  }
 })
