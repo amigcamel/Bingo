b991552d8a02bc77012c5792b13c6332eed4f5ba
amigcamel <amigcamel@gmail.com>
2020-12-13 18:48:58 +0800

    rewrite with Vue component

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Bingo/>
+  </div>
+</template>
+
+<script>
+import Bingo from './components/Bingo.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Bingo,
+  },
+};
+</script>
