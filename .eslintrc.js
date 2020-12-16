ee651c4c0af586022a7a6822709e3ec90ba19487
amigcamel <amigcamel@gmail.com>
2020-12-16 17:46:04 +0800

    add .env.production; use vue-eslint-parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
-  parser: 'babel-eslint',
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
