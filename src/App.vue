2ac27b9e99669255b015a7f77042e41e41d07135
amigcamel <amigcamel@gmail.com>
2021-01-04 17:50:57 +0800

    put footer in vue scope

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
     <router-view/>
+    <div class="footer">
+      <div class="container text-center">
+        &copy; Copyright {{new Date().getFullYear()}},
+        <a href="https://aji.tw" target="_blank">Owen Liu</a>. All Rights Reserved
+      </div>
+    </div>
   </div>
+
 </template>
